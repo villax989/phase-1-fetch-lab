6bbab4278e7d2c8be21ba05fb80775a44441acb1
xavier <villax989@icloud.com>
2024-05-03 08:49:55 -0400

    done

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+      .then((resp) => resp.json())
+      .then((json) => {
+          renderBooks(json);
+          return json; // Don't forget to return the data
+      })
+      .catch((error) => {
+          console.error("Error fetching books:", error);
+      });
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
   });
 }
 
